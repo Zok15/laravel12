268fbedfc16589d14caedfef38274bd2f24ee00e
Катерина Сальникова <myasoezhka@gmail.com>
2026-01-14 17:33:20 +0400

    hold off v1

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,15 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(base_path('routes/api/v1.php'));
+//Route::prefix('v1')->group(base_path('routes/api/v1.php'));
 Route::prefix('v2')->middleware('auth:sanctum')->group(base_path('routes/api/v2.php'));
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
     Route::post('/register', RegisterController::class);
     Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
